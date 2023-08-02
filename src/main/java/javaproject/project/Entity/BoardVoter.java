336aaf9c32b0d1406fee75b3051dd316cff00503
JDH0411 <ejrghks112500@gmail.com>
2023-08-02 15:40:22 +0900

    first commit

---
+++ src/main/java/javaproject/project/Entity/BoardVoter.java
@@ -0,0 +1,11 @@
+package javaproject.project.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardVoter {
+    private Integer boardId;
+    private Integer voterId;
+}
